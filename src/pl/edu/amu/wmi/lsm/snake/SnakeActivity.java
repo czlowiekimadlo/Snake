589c69c26e69eb3fdbb8f3a146b7a525744859c4
Wojciech Chojnacki <quak@tlen.pl>
2012-05-07 19:29:06 +0200

    Project setup

---
+++ src/pl/edu/amu/wmi/lsm/snake/SnakeActivity.java
@@ -0,0 +1,13 @@
+package pl.edu.amu.wmi.lsm.snake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SnakeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
