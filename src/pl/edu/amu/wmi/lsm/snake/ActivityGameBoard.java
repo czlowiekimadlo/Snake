cdaa1a66c6bafb584dd128b133eccf610898d927
unknown <AnitkA@AnitkA-PC.(none)>
2012-06-12 14:28:53 +0200

    game board: poczatek

---
+++ src/pl/edu/amu/wmi/lsm/snake/ActivityGameBoard.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.edu.amu.wmi.lsm.snake;
+
+/**
+ *
+ * @author AnitkA
+ */
+public class ActivityGameBoard {
+
+}
